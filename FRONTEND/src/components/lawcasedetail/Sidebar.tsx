752d8215550ce5374b994150880c9a6175bda907
최근영 <wwaall2626@naver.com>
2024-03-04 17:40:22 +0900

    feat : 사이드바 구현중 UI

--- FRONTEND/src/components/lawcasedetail/Sidebar.tsx
+++ FRONTEND/src/components/lawcasedetail/Sidebar.tsx
@@ -1,9 +1,31 @@
+import style from "../../styles/lawcasedetail/LawCaseSidebar.module.css";
+import ARROW from "../../assets/arrow.png"; /* <a href="https://www.flaticon.com/kr/free-icons/" title="화살 아이콘">화살 아이콘  제작자: Catalin Fertu - Flaticon</a> */
+import { useState } from "react";
+
 const Sidebar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [isSelected, setIsselected] = useState<boolean>(false);
+  return (
+    <article
+      className={
+        isSelected ? `${style["sidebar"]} ${style["hide"]}` : style["sidebar"]
+      }
+    >
+      <div
+        className={style["side-btn"]}
+        onClick={() => setIsselected(!isSelected)}
+      >
+        <img
+          className={
+            isSelected
+              ? `${style["rotate"]} ${style["side-btn-img"]}`
+              : style["side-btn-img"]
+          }
+          src={ARROW}
+          alt=""
+        />
+      </div>
+    </article>
+  );
 };
 
 export default Sidebar;
