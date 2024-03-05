d1438dde4eb4a08bbaa2f3b6cd3bd658378fc904
정소영 <so2043@naver.com>
2024-03-05 09:45:31 +0900

    Refactor : 법령검색 api 수정
    BaseResponse 추가 및 법령검색 api 로직 수정

--- backend/law_service/src/main/java/com/sobolaw/api/statute/service/StatuteService.java
+++ backend/law_service/src/main/java/com/sobolaw/api/statute/service/StatuteService.java
@@ -1,20 +1,44 @@
 package com.sobolaw.api.statute.service;
 
+import com.sobolaw.api.statute.dto.StatuteSearchDTO;
+import com.sobolaw.api.statute.entity.Statute;
 import com.sobolaw.api.statute.repository.StatuteRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
+@RequiredArgsConstructor
 @Service
 public class StatuteService {
 
     private final StatuteRepository statuteRepository;
 
-    public StatuteService(StatuteRepository statuteRepository) {
-        this.statuteRepository = statuteRepository;
+    public List<StatuteSearchDTO> searchByKeyword(String searchKeyword) {
+
+        List<Statute> statutes = statuteRepository.findByStatuteNameContaining(searchKeyword);
+
+        if (statutes.isEmpty()) {
+            throw new IllegalArgumentException("해당 키워드 관련 법령이 없습니다. searchKeyword=" + searchKeyword);
+        }
+
+        return statutes.stream()
+            .map(this::convertToStatuteSearchDTO)
+            .collect(Collectors.toList());
     }
 
-    public List<Object[]> getSearchStatute(String searchKeyword) {
-        return statuteRepository.getSearchStatute(searchKeyword);
+    private StatuteSearchDTO convertToStatuteSearchDTO(Statute statute) {
+        return new StatuteSearchDTO(
+            statute.getStatuteId(),
+            statute.getAmendmentType(),
+            statute.getDepartment(),
+            statute.getEnforcementDate(),
+            statute.getPublicationDate(),
+            statute.getPublicationNumber(),
+            statute.getStatuteName(),
+            statute.getStatuteNumber(),
+            statute.getStatuteType()
+        );
     }
+
 }
