4a51e28aa5f49d63b29b0329b3756d2de9fda095
정소영 <so2043@naver.com>
2024-03-06 17:56:07 +0900

    Feat : 관심판례 목록 조회 코드 변경
    - RequestParam에서 RequestBody으로 변경
    - Map으로 구현

--- backend/law_service/src/main/java/com/sobolaw/api/precedent/controller/PrecedentController.java
+++ backend/law_service/src/main/java/com/sobolaw/api/precedent/controller/PrecedentController.java
@@ -6,12 +6,14 @@ import com.sobolaw.api.precedent.service.PrecedentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/precedent")
@@ -28,9 +30,10 @@ public class PrecedentController {
         return BaseResponse.success(HttpStatus.OK.value(), "판례 상세 조회 성공!", precedent);
     }
 
-    @GetMapping("/list")
+    @PostMapping("/list")
     @Operation(summary = "관심판례 목록 조회", description = "해당 판례ID 배열로 판례 목록 조회")
-    public BaseResponse<List<PrecedentDTO>> getPrecedentList(@RequestParam List<Long> precedentIds) {
+    public BaseResponse<List<PrecedentDTO>> getPrecedentList(@RequestBody Map<String, List<Long>> requestBody) {
+        List<Long> precedentIds = requestBody.get("precedentId");
         List<PrecedentDTO> precedents = precedentService.findPrecedentsById(precedentIds);
         return BaseResponse.success(HttpStatus.OK.value(), "관심판례 목록 조회 성공!", precedents);
     }
