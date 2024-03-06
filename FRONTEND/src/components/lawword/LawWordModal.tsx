69ca36a43424ca1720bba58186012af689459a29
최근영 <wwaall2626@naver.com>
2024-03-06 15:48:53 +0900

    feat : 챗봇 UI 모달 구현
    - 챗봇 탭 이동시 제목 바꿔야함
    - 라우터 정리 한번 진행해야 챗봇모달 새로 열림

--- FRONTEND/src/components/lawword/LawWordModal.tsx
+++ FRONTEND/src/components/lawword/LawWordModal.tsx
@@ -1,10 +1,12 @@
-import { Flex, Button, Input } from "antd";
+import { Flex, Button } from "antd";
 import style from "../../styles/lawword/LawWordModal.module.css";
 import DICTIONARY from "../../assets/dictionary.png";
 import CHAT from "../../assets/chat.png";
 import CHARACTER from "../../assets/character.png";
 import CLOSE from "../../assets/close.png";
-import LawWordCard from "./LawWordCard";
+import { useState } from "react";
+import LawWordDict from "./LawWordDict";
+import LawWordChat from "./LawWordChat";
 
 {
   /* <a target="_blank" href="https://icons8.com/icon/35087/dictionary">사전</a> 작가: <a target="_blank" href="https://icons8.com">Icons8</a>
@@ -17,7 +19,8 @@ interface LawWordModalProps {
 }
 
 const LawWordModal = ({ modalHandler }: LawWordModalProps) => {
-  const { Search } = Input;
+  const [currentSelect, setCurrentSelect] = useState<boolean>(true);
+
   return (
     <section className={style["modal-page"]}>
       <Flex vertical>
@@ -31,33 +34,36 @@ const LawWordModal = ({ modalHandler }: LawWordModalProps) => {
             onClick={modalHandler}
           />
         </Flex>
-        <Flex
-          className={style["modal-body"]}
-          justify="space-around"
-          align="center"
-          vertical
-        >
-          <Search
-            placeholder="검색할 단어를 입력하세요. "
-            style={{ width: "90%" }}
-          />
-          <Flex
-            className={style["modal-body__content"]}
-            vertical
-            align="center"
-          >
-            <LawWordCard></LawWordCard>
-          </Flex>
-        </Flex>
+        {currentSelect ? (
+          <LawWordDict></LawWordDict>
+        ) : (
+          <LawWordChat></LawWordChat>
+        )}
         <Flex className={style["footer-btn"]}>
-          <Button type="primary" className={style["footer-btn__1"]}>
+          <Button
+            type="primary"
+            className={
+              currentSelect
+                ? `${style["active"]} ${style["footer-btn__1"]}`
+                : style["footer-btn__1"]
+            }
+            onClick={() => setCurrentSelect(true)}
+          >
             <img
               className={style["footer-btn__image"]}
               src={DICTIONARY}
               alt=""
             />
           </Button>
-          <Button type="primary" className={style["footer-btn__2"]}>
+          <Button
+            type="primary"
+            className={
+              currentSelect
+                ? style["footer-btn__2"]
+                : `${style["active"]} ${style["footer-btn__2"]}`
+            }
+            onClick={() => setCurrentSelect(false)}
+          >
             <img className={style["footer-btn__image"]} src={CHAT} alt="" />
           </Button>
         </Flex>
