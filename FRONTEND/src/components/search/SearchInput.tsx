884712e77bb7dc2ca34550bb16cb7c90e662c620
조성호 <whtjdgh510@naver.com>
2024-03-06 17:09:53 +0900

    feat: law search page 진행중

--- FRONTEND/src/components/search/SearchInput.tsx
+++ FRONTEND/src/components/search/SearchInput.tsx
@@ -1,18 +1,54 @@
-// SearchInput.tsx
 import React, { useState } from 'react';
-import { Input } from 'antd';
+import { Input, Tabs } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import style from '../../styles/SearchInput.module.css';
 
-const SearchInput: React.FC<{ onSearch: (value: string) => void }> = ({ onSearch }) => {
+// Props 타입 정의
+interface SearchInputProps {
+  onSearch: (value: string, activeTab: string) => void;
+}
+
+const SearchInput: React.FC<SearchInputProps> = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [activeTab, setActiveTab] = useState<string>('1');
+
+  const handleSearch = (): void => {
+    onSearch(searchTerm, activeTab);
+  };
+  
+  const onTabChange = (activeKey: string): void => {
+    setActiveTab(activeKey);
+  }
+
+  const tabItems = [
+    {
+      label: '판례 검색',
+      key: 'lawcase',
+    },
+    {
+      label: '법령 검색',
+      key: 'law',
+    },
+  ]
 
   return (
-    <div className={style.customSearchInput}>
-      <Input.Search
+    <div className={style.container}>
+      <h3 className={style.title}>
+        고도화된 검색 환경으로<br />99,999,999 건의 판례를 찾아보세요.
+      </h3>
+      <Tabs
+        defaultActiveKey={activeTab}
+        onChange={onTabChange}
+        items={tabItems}
+        tabBarGutter={40}
+      />
+      <Input
+        prefix={<SearchOutlined style={{ fontSize: '20px', padding: '0px 20px'}}/>}
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
+        onPressEnter={() => handleSearch()}
+        className={style.searchInput}
         placeholder="키워드를 검색하세요"
-        onSearch={onSearch} // 'onSearch' 콜백은 사용자가 검색 버튼을 클릭하거나 엔터 키를 눌렀을 때 호출됩니다.
       />
     </div>
   );
--- FRONTEND/src/pages/SearchPage.tsx
+++ FRONTEND/src/pages/SearchPage.tsx
@@ -1,21 +1,17 @@
 // SearchPage.tsx
-import React, { useState } from 'react';
-import { Layout, Typography, Row, Col } from 'antd';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Layout, Row, Col } from 'antd';
+import { BookOutlined, CommentOutlined, StarOutlined } from '@ant-design/icons';
 import SearchInput from '../components/search/SearchInput';
 import SearchResultList from '../components/search/SearchResultList';
 import pageStyle from '../styles/SearchPage.module.css';
 
 const { Content } = Layout;
-const { Title } = Typography;
 
 const SearchPage: React.FC = () => {
-  const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
 
-  const handleSearch = (value: string) => {
-    // 여기에 검색 로직을 구현하세요.
-    // 예: API 호출 및 검색 결과 상태 업데이트
-    console.log('검색 실행:', value);
-  };
 
   return (
     <Layout>
@@ -23,15 +19,25 @@ const SearchPage: React.FC = () => {
         <Row gutter={[16, 16]} justify="center">
           <Col xs={24} sm={24} md={16} lg={16} xl={16}>
             <div className={pageStyle.searchContainer}>
-              <Title level={3} style={{ color: 'white', textAlign: 'center', marginBottom: '7rem'}}>
-                고도화된 검색 환경으로<br />99,999,999 건의 판례를 찾아보세요.
-              </Title>
-              <SearchInput onSearch={handleSearch} />
+              <SearchInput />
             </div>
+            <div className={pageStyle.iconContainer}>
+              <div className={pageStyle.iconWrapper} onClick={() => navigate('/cases')}>
+                <BookOutlined className={pageStyle.icon} />
+                <div className={pageStyle.iconText}>판례</div>
+              </div>
+              <div className={pageStyle.iconWrapper} onClick={() => navigate('/laws')}>
+                <CommentOutlined className={pageStyle.icon} />
+                <div className={pageStyle.iconText}>법령</div>
+              </div>
+              <div className={pageStyle.iconWrapper} onClick={() => navigate('/favorites')}>
+                <StarOutlined className={pageStyle.icon} />
+                <div className={pageStyle.iconText}>스크랩</div>
+              </div>
+            </div>
+            <SearchResultList searchResults={[]}/>
           </Col>
         </Row>
-        
-        <SearchResultList searchResults={searchResults} />
       </Content>
     </Layout>
   );
