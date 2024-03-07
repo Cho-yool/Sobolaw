7199df376014926462a06a3fb30e82de15434533
정소영 <so2043@naver.com>
2024-03-07 11:21:37 +0900

    Refactor : 법령 검색 api 수정
    PathVariable으로 어노테이션 변경

--- backend/law_service/src/main/java/com/sobolaw/api/statute/controller/StatuteController.java
+++ backend/law_service/src/main/java/com/sobolaw/api/statute/controller/StatuteController.java
@@ -6,26 +6,24 @@ import com.sobolaw.api.statute.service.StatuteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "StatuteController", description = "법령 검색 기능")
 @RestController
-@RequestMapping
+@RequestMapping("/statute")
+@Tag(name = "StatuteController", description = "법령 관련 기능")
+@RequiredArgsConstructor
 public class StatuteController {
 
     private final StatuteService statuteService;
 
-    public StatuteController(StatuteService statuteService) {
-        this.statuteService = statuteService;
-    }
-
-    @GetMapping("/statute/search")
+    @GetMapping("/search/{searchKeyword}")
     @Operation(summary = "법령 검색", description = "키워드를 사용하여 법령 타이틀을 검색합니다.")
-    public BaseResponse<List<StatuteSearchDTO>> searchStatutes(@RequestParam String searchKeyword) {
+    public BaseResponse<List<StatuteSearchDTO>> searchStatutes(@PathVariable String searchKeyword) {
         List<StatuteSearchDTO> searchResults = statuteService.searchByKeyword(searchKeyword);
         return BaseResponse.success(HttpStatus.OK.value(), "법령 검색 성공!",searchResults);
     }
