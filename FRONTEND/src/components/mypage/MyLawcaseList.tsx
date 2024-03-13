07dcd1e88c9b4f7507dfba8f5eeb3cb84f843b97
김현지 <hanguel97@gmail.com>
2024-03-13 11:01:03 +0900

    test: 소장조회 axios 요청 조회

--- FRONTEND/src/components/mypage/MyLawcaseList.tsx
+++ FRONTEND/src/components/mypage/MyLawcaseList.tsx
@@ -1,6 +1,182 @@
-import { Table } from "antd";
+import { Table, Space, Tag } from "antd";
 import type { TableProps } from "antd";
+import {
+  MypaperWide,
+  MypaperNarrow,
+  MemberLawsuit,
+} from "../../types/DataTypes";
 
-export default function MyLawcaseTable(params: type) {
-  return <div></div>;
+const columnsWide: TableProps<MypaperWide>["columns"] = [
+  {
+    title: "임시저장명",
+    dataIndex: "name",
+    key: "name",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "피고소인",
+    dataIndex: "target",
+    key: "target",
+  },
+  {
+    title: "작성일자",
+    dataIndex: "date",
+    key: "date",
+  },
+  {
+    title: "소장분류",
+    key: "tags",
+    dataIndex: "tags",
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? "geekblue" : "green";
+          if (tag === "Fraud") {
+            color = "magenta";
+          } else if (tag === "Insult") {
+            color = "cyan";
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: "설정",
+    key: "action",
+    render: () => (
+      <Space size="middle">
+        <a>수정</a>
+        <a>삭제</a>
+      </Space>
+    ),
+  },
+];
+
+const columnsNarrow: TableProps<MypaperNarrow>["columns"] = [
+  {
+    title: "임시저장명",
+    dataIndex: "name",
+    key: "name",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "설정",
+    key: "action",
+    render: () => (
+      <Space size="middle">
+        <a>수정</a>
+        <a>삭제</a>
+      </Space>
+    ),
+  },
+];
+
+const data: MypaperWide[] = [
+  {
+    key: "1",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "2",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "3",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "4",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "5",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "6",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "7",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "8",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "9",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "10",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "11",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+  {
+    key: "12",
+    name: "정의란무엇인가",
+    target: "김종범",
+    date: "2024-03-06",
+    tags: ["명예훼손"],
+  },
+];
+
+interface MyLawcaseTableProps {
+  lawsuitList: MemberLawsuit[] | undefined; // 또는 | null 추가 가능, 데이터가 없을 수도 있음을 고려
+}
+
+export default function MyLawcaseTable({ lawsuitList }: MyLawcaseTableProps) {
+  console.log(lawsuitList);
+  // const dataWide;
+  // const dataNarrow;
+
+  return (
+    <>
+      <Table columns={columnsWide} dataSource={data} />
+
+      <Table columns={columnsNarrow} dataSource={data} />
+    </>
+  );
 }
--- FRONTEND/src/pages/mypage/MyPaper.tsx
+++ FRONTEND/src/pages/mypage/MyPaper.tsx
@@ -1,19 +1,15 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Space, Table, Tag, Button, Divider, Row, Col } from "antd";
-import type { TableProps } from "antd";
-import {
-  MypaperWide,
-  MypaperNarrow,
-  MemberLawsuit,
-} from "../../types/DataTypes";
+import { Button, Divider, Row, Col } from "antd";
+import { MemberLawsuit } from "../../types/DataTypes";
 import { getLawsuitList } from "../../api/lawsuit";
 import style from "../../styles/mypage/Mypaper.module.css";
 import "../../App.css";
+import MyLawcaseTable from "../../components/mypage/MyLawcaseList";
 
 export default function Mypaper() {
   const navigate = useNavigate();
-  const [lawsuitList, setLawsuitList] = useState<MemberLawsuit>();
+  const [lawsuitList, setLawsuitList] = useState<MemberLawsuit[]>();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,162 +19,16 @@ export default function Mypaper() {
     fetchData();
   }, []);
 
-  console.log(lawsuitList);
-
-  const columnsWide: TableProps<MypaperWide>["columns"] = [
-    {
-      title: "임시저장명",
-      dataIndex: "name",
-      key: "name",
-      render: (text) => <a>{text}</a>,
-    },
-    {
-      title: "피고소인",
-      dataIndex: "target",
-      key: "target",
-    },
-    {
-      title: "작성일자",
-      dataIndex: "date",
-      key: "date",
-    },
-    {
-      title: "소장분류",
-      key: "tags",
-      dataIndex: "tags",
-      render: (_, { tags }) => (
-        <>
-          {tags.map((tag) => {
-            let color = tag.length > 5 ? "geekblue" : "green";
-            if (tag === "loser") {
-              color = "volcano";
-            }
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })}
-        </>
-      ),
-    },
-    {
-      title: "설정",
-      key: "action",
-      render: () => (
-        <Space size="middle">
-          <a>수정</a>
-          <a>삭제</a>
-        </Space>
-      ),
-    },
-  ];
-
-  const columnsNarrow: TableProps<MypaperNarrow>["columns"] = [
-    {
-      title: "임시저장명",
-      dataIndex: "name",
-      key: "name",
-      render: (text) => <a>{text}</a>,
-    },
-    {
-      title: "설정",
-      key: "action",
-      render: () => (
-        <Space size="middle">
-          <a>수정</a>
-          <a>삭제</a>
-        </Space>
-      ),
-    },
-  ];
-
-  const data: MypaperWide[] = [
-    {
-      key: "1",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "2",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "3",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "4",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "5",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "6",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "7",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "8",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "9",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "10",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "11",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-    {
-      key: "12",
-      name: "정의란무엇인가",
-      target: "김종범",
-      date: "2024-03-06",
-      tags: ["명예훼손"],
-    },
-  ];
+  // useEffect(() => {
+  //   getLawsuitList(1)
+  //     .then((res) => {
+  //       console.log(res);
+  //       setLawsuitList(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
   return (
     <div className="pages">
@@ -199,11 +49,11 @@ export default function Mypaper() {
         <Row>
           <Col xs={0} sm={0} md={24} lg={24}>
             <div className={style["table"]}>
-              <Table columns={columnsWide} dataSource={data} />
+              <MyLawcaseTable lawsuitList={lawsuitList} />
             </div>
           </Col>
           <Col xs={24} sm={24} md={0}>
-            <Table columns={columnsNarrow} dataSource={data} />
+            <MyLawcaseTable lawsuitList={lawsuitList} />
           </Col>
         </Row>
       </div>
