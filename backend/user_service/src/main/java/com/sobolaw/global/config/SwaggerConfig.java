992c68e2d4165899e6594ce845a21278b13092ac
김종범 <kbumk1234@naver.com>
2024-03-22 10:19:32 +0900

    fix: user-service swager에 authorization 추가

--- backend/user_service/src/main/java/com/sobolaw/global/config/SwaggerConfig.java
+++ backend/user_service/src/main/java/com/sobolaw/global/config/SwaggerConfig.java
@@ -6,8 +6,11 @@ import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -33,12 +36,19 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .addServersItem(new Server().url("https://j10a604.p.ssafy.io/api/user-service").description("요청 서버"))
-            .info(new io.swagger.v3.oas.models.info.Info()
-                .title("User-Service")
-                .version("1.0")
-                .description("User 기능 당당")
-                .contact(new Contact().name("장재성").email("")));
+                .addServersItem(new Server().url("https://j10a604.p.ssafy.io/api/user-service").description("요청 서버"))
+                .info(new Info()
+                        .title("User-Service")
+                        .version("1.0")
+                        .description("User 기능 당당")
+                        .contact(new Contact().name("장재성").email("")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new io.swagger.v3.oas.models.security.SecurityScheme()
+                                .name("bearerAuth")
+                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 
     @Operation(summary = "Naver OAuth2 Authorization Endpoint", description = "Endpoint for initiating OAuth2 authorization with Naver.")
