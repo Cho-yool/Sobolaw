4db62b2bb15807ced8bcea4be4c2d141bd8e1d55
정소영 <so2043@naver.com>
2024-03-29 14:17:07 +0900

    Fix : ILM파일 실행 오류
    필요한 상황에서만 실행 되도록 변경

--- backend/law_service/src/main/java/com/sobolaw/ilm/ElasticsearchILM.java
+++ backend/law_service/src/main/java/com/sobolaw/ilm/ElasticsearchILM.java
@@ -3,25 +3,21 @@ package com.sobolaw.ilm;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.RestClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 @Slf4j
-public class ElasticsearchILM implements CommandLineRunner {
+public class ElasticsearchILM {
 
     private RestClient restClient;
-    @Autowired
-    public ElasticsearchILM(RestClient restClient) {
-        this.restClient = restClient;
-    }
-    @Override
-    public void run(String... args) throws Exception {
+
+    public void applyILMPolicy() throws Exception {
         // ILM 정책 JSON 파일 읽기
         String ilmPolicyJson = new String(Files.readAllBytes(Paths.get(new ClassPathResource("elastic/ilm-policy.json").getURI())), StandardCharsets.UTF_8);
 
