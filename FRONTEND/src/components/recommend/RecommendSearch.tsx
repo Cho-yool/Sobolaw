008cefb97222f453b0e08c8809ced79253654a08
whtjdgh510 <whtjdgh510@naver.com>
2024-03-31 19:31:20 +0900

    fix: RecommendTextMessage 수정

--- FRONTEND/src/components/recommend/RecommendSearch.tsx
+++ FRONTEND/src/components/recommend/RecommendSearch.tsx
@@ -1,25 +1,30 @@
-import React, { useState, ReactNode } from 'react';
-import { Select, Input, Button, Steps } from 'antd';
-import { ClusterOutlined, HomeOutlined, CarOutlined, UserOutlined } from '@ant-design/icons';
-import { useNavigate } from 'react-router-dom';
-import RecommendTextMessage from './RecommendTextMessage';
-import style from '../../styles/recommend/RecommendSearch.module.css';
-import LoadingImage from './LoadingImage';
-import { searchPrecedents } from '../../api/recommendsearch';
+import React, { useState, ReactNode } from "react";
+import { Select, Input, Button, Steps } from "antd";
+import {
+  ClusterOutlined,
+  HomeOutlined,
+  CarOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import RecommendTextMessage from "./RecommendTextMessage";
+import style from "../../styles/recommend/RecommendSearch.module.css";
+import LoadingImage from "./LoadingImage";
+import { searchPrecedents } from "../../api/recommendsearch";
 
 const { Option } = Select;
 const { TextArea } = Input;
 
 type Message = {
   text: ReactNode;
-  type: 'sent' | 'received';
-}
+  type: "sent" | "received";
+};
 
 const RecommendSearch: React.FC = () => {
-  const [selectedCaseType, setSelectedCaseType] = useState<string>('');
-  const [stepTwoValue, setStepTwoValue] = useState<string>('');
-  const [stepThreeValue, setStepThreeValue] = useState<string>('');
-  const [stepFourValue, setStepFourValue] = useState<string>('');
+  const [selectedCaseType, setSelectedCaseType] = useState<string>("");
+  const [stepTwoValue, setStepTwoValue] = useState<string>("");
+  const [stepThreeValue, setStepThreeValue] = useState<string>("");
+  const [stepFourValue, setStepFourValue] = useState<string>("");
   const [currentStep, setCurrentStep] = useState<number>(1);
   const [isLoading, setIsLoading] = useState(false); // 새로운 상태로 로딩 상태를 추가합니다.
   const [progress, setProgress] = useState(0); // 새로운 상태로 로딩 진행률을 추가합니다.
@@ -33,21 +38,27 @@ const RecommendSearch: React.FC = () => {
     }
   };
 
-  const handleStepTwoChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleStepTwoChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
     setStepTwoValue(event.target.value);
     if (event.target.value.length >= 6 && currentStep === 2) {
       setCurrentStep(currentStep + 1);
     }
   };
 
-  const handleStepThreeChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleStepThreeChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
     setStepThreeValue(event.target.value);
     if (event.target.value.length >= 6 && currentStep === 3) {
       setCurrentStep(currentStep + 1);
     }
   };
 
-  const handleStepFourChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleStepFourChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
     setStepFourValue(event.target.value);
     if (event.target.value.length >= 6 && currentStep === 4) {
       setCurrentStep(currentStep + 1);
@@ -56,59 +67,72 @@ const RecommendSearch: React.FC = () => {
 
   // Message 내용을 별도의 함수로 정의합니다.
   const placeMessages: Message[] = [
-    { text: '사건장소는 어떻게 입력해야 하나요?', type: 'sent' },
+    { text: "사건장소는 어떻게 입력해야 하나요?", type: "sent" },
     {
       text: (
         <>
-          6자 이상 입력해주세요.<br />
+          6자 이상 입력해주세요.
+          <br />
           예: 회사, 건물, 주택, 농지, <br />
-          아파트, 은행, 도로, 자동차 등< br/>
+          아파트, 은행, 도로, 자동차 등<br />
           실제 지명 보다 장소의 특성 또는 <br />
-          장소의 카테고리를 입력하는 게 좋습니다.<br />
-          예를 들어, OO기업(X) ⭢ 회사(O)<br />
+          장소의 카테고리를 입력하는 게 좋습니다.
+          <br />
+          예를 들어, OO기업(X) ⭢ 회사(O)
+          <br />
           OO아파트(X) ⭢ 아파트(O)
         </>
       ),
-      type: 'received',
+      type: "received",
     },
   ];
-  
+
   const objectMessages: Message[] = [
-    { text: '사건대상은 어떻게 입력해야 하나요?', type: 'sent' },
+    { text: "사건대상은 어떻게 입력해야 하나요?", type: "sent" },
     {
       text: (
         <>
-          6자 이상 입력해주세요.<br />
+          6자 이상 입력해주세요.
+          <br />
           예: 토지, 등기, 보험, 채권, 부동산, 소유권, 항고, 자동차 등<br />
-          실제 대상명 보다 대상의 특성 또는 대상의 카테고리를 입력하는 것이 좋습니다.<br />
-          이를 통해 추천 검색 결과의 정확도를 높일 수 있습니다.<br />
-          예를 들어, OO로 OO(X) ⭢ 토지(O)<br />
+          실제 대상명 보다 대상의 특성 또는 대상의 카테고리를 입력하는 것이
+          좋습니다.
+          <br />
+          이를 통해 추천 검색 결과의 정확도를 높일 수 있습니다.
+          <br />
+          예를 들어, OO로 OO(X) ⭢ 토지(O)
+          <br />
           OO차,OO원(X) ⭢ 자동차, 자산(O)
         </>
       ),
-      type: 'received',
+      type: "received",
     },
   ];
-  
+
   const personMessages: Message[] = [
-    { text: '사건인물은 어떻게 입력해야 하나요?', type: 'sent' },
+    { text: "사건인물은 어떻게 입력해야 하나요?", type: "sent" },
     {
       text: (
         <>
-          6자 이상 입력해주세요.<br />
-          예: 채무자, 채권자, 근로자, 참가인, 조합원, 피해자, 소유자 등 인물의 실명보다 인물의 역할, 관계를 입력하는 것이 좋습니다.<br />
-          이를 통해 추천 검색 결과의 정확도를 높일 수 있습니다.<br />
-          예를 들어, 김OO(X) ⭢ 채무자(O)<br />
+          6자 이상 입력해주세요.
+          <br />
+          예: 채무자, 채권자, 근로자, 참가인, 조합원, 피해자, 소유자 등 인물의
+          실명보다 인물의 역할, 관계를 입력하는 것이 좋습니다.
+          <br />
+          이를 통해 추천 검색 결과의 정확도를 높일 수 있습니다.
+          <br />
+          예를 들어, 김OO(X) ⭢ 채무자(O)
+          <br />
           황OO씨(X) ⭢ 피해자(O)
         </>
       ),
-      type: 'received',
+      type: "received",
     },
   ];
 
   const handleSubmit = async () => {
     const situation = `${selectedCaseType} ${stepTwoValue} ${stepThreeValue} ${stepFourValue}`;
-    console.log('Submitting', situation);
+    console.log("Submitting", situation);
 
     setIsLoading(true); // 로딩 상태를 true로 설정
     setProgress(0); // 진행률을 0으로 초기화
@@ -116,7 +140,7 @@ const RecommendSearch: React.FC = () => {
     try {
       // simulateLoadingProcess(); // 로딩 프로세스를 시뮬레이션하는 함수 호출
       const searchResults = await searchPrecedents(situation);
-      console.log('Search results:', searchResults);
+      console.log("Search results:", searchResults);
       // TODO: 검색 결과 페이지로 이동
       // 예: 결과 페이지로 이동하면서 검색 결과 데이터 전달
 
@@ -128,7 +152,9 @@ const RecommendSearch: React.FC = () => {
             setTimeout(() => {
               setIsLoading(false);
               window.scrollTo(0, 0);
-              navigate('/recommend-results', { state: { searchResults: searchResults.data } });
+              navigate("/recommend-results", {
+                state: { searchResults: searchResults.data },
+              });
             }, 1500);
             return 100;
           }
@@ -136,13 +162,13 @@ const RecommendSearch: React.FC = () => {
         });
       }, 70);
     } catch (error) {
-      console.error('검색 중 오류가 발생하였습니다', error);
+      console.error("검색 중 오류가 발생하였습니다", error);
       setIsLoading(false);
     }
-  }
+  };
 
   if (isLoading) {
-    return <LoadingImage progress={progress} />
+    return <LoadingImage progress={progress} />;
   }
 
   return (
@@ -151,26 +177,26 @@ const RecommendSearch: React.FC = () => {
         <Steps
           direction="vertical"
           current={currentStep - 1}
-          style={{ height: '35%', fontWeight: 'bold' }}
-          size='small'
+          style={{ height: "35%", fontWeight: "bold" }}
+          size="small"
           items={[
             {
-              style: { height: '32%' },
-              title: '사건 분류',
+              style: { height: "32%" },
+              title: "사건 분류",
               icon: <ClusterOutlined />,
             },
             {
-              style: { height: '42%' },
-              title: '사건 장소',
+              style: { height: "42%" },
+              title: "사건 장소",
               icon: <HomeOutlined />,
             },
             {
-              style: { height: '42%' },
-              title: '사건 대상',
+              style: { height: "42%" },
+              title: "사건 대상",
               icon: <CarOutlined />,
             },
             {
-              title: '사건 인물',
+              title: "사건 인물",
               icon: <UserOutlined />,
             },
           ]}
@@ -185,9 +211,11 @@ const RecommendSearch: React.FC = () => {
             optionFilterProp="children"
             onChange={handleCaseTypeChange}
             filterOption={(input, option) =>
-              (option!.children as unknown as string).toLowerCase().includes(input.toLowerCase())
+              (option!.children as unknown as string)
+                .toLowerCase()
+                .includes(input.toLowerCase())
             }
-            style={{ width: '15%', height: '50px', fontSize: '15px' }}
+            style={{ width: "15%", height: "50px", fontSize: "15px" }}
           >
             <Option value="손해배상">손해배상</Option>
             <Option value="사기">사기</Option>
@@ -198,7 +226,9 @@ const RecommendSearch: React.FC = () => {
             <Option value="소유권분쟁">소유권분쟁</Option>
             <Option value="기타">기타</Option>
           </Select>
-          <RecommendTextMessage messages={placeMessages} />
+          <div className={style.recommendTextMessageWrapper}>
+            <RecommendTextMessage messages={placeMessages} />
+          </div>
         </div>
         {currentStep > 1 && (
           <div className={`${style.searchItem} ${style.fadeInUp}`}>
@@ -208,11 +238,12 @@ const RecommendSearch: React.FC = () => {
                 value={stepTwoValue}
                 placeholder="예) 회사, 건물, 주택, 농지, 아파트, 은행, 도로, 자동차 등"
                 onChange={handleStepTwoChange}
-                style={{ width: '50%', height: '100%' }}
+                style={{ width: "50%", height: "100%" }}
                 autoSize={{ minRows: 5, maxRows: 5 }}
               />
-              <RecommendTextMessage messages={objectMessages} />
-
+              <div className={style.recommendTextMessageWrapper}>
+                <RecommendTextMessage messages={objectMessages} />
+              </div>
             </div>
           </div>
         )}
@@ -224,11 +255,12 @@ const RecommendSearch: React.FC = () => {
                 value={stepThreeValue}
                 placeholder="예)  토지, 등기, 보험, 채권, 부동산, 소유권, 항고, 자동차 등"
                 onChange={handleStepThreeChange}
-                style={{ width: '50%', height: '100%' }}
+                style={{ width: "50%", height: "100%" }}
                 autoSize={{ minRows: 5, maxRows: 5 }}
               />
-              <RecommendTextMessage messages={personMessages} />
-
+              <div className={style.recommendTextMessageWrapper}>
+                <RecommendTextMessage messages={personMessages} />
+              </div>
             </div>
           </div>
         )}
@@ -240,7 +272,7 @@ const RecommendSearch: React.FC = () => {
                 value={stepFourValue}
                 placeholder="예) 채무자, 채권자, 근로자, 참가인, 조합원, 피해자, 소유자 등"
                 onChange={handleStepFourChange}
-                style={{ width: '50%', height: '100%' }}
+                style={{ width: "50%", height: "100%" }}
                 autoSize={{ minRows: 5, maxRows: 5 }}
               />
             </div>
@@ -248,7 +280,18 @@ const RecommendSearch: React.FC = () => {
         )}
         {currentStep > 4 && (
           <div className={`${style.searchItem} ${style.fadeInUp}`}>
-            <Button type="primary" onClick={handleSubmit} style={{ width: 100, height: 50, fontSize: 18, marginLeft: '39%' }}>제출</Button>
+            <Button
+              type="primary"
+              onClick={handleSubmit}
+              style={{
+                width: 100,
+                height: 50,
+                fontSize: 18,
+                marginLeft: "39%",
+              }}
+            >
+              제출
+            </Button>
           </div>
         )}
       </div>
