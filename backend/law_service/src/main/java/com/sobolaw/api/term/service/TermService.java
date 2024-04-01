c00921e00e5e68995f5cf04094bf967bf18c07a3
정소영 <so2043@naver.com>
2024-04-01 17:55:43 +0900

    Feat : 법령용어명 목록 조회 api 추가
    법령용어명 목록 조회 api 추가

--- backend/law_service/src/main/java/com/sobolaw/api/term/service/TermService.java
+++ backend/law_service/src/main/java/com/sobolaw/api/term/service/TermService.java
@@ -83,4 +83,8 @@ public class TermService {
             entity.getTermDefinition()
         );
     }
+
+    public List<String> findDistinctTermNames() {
+        return termRepository.findAllDistinctTermNames();
+    }
 }
