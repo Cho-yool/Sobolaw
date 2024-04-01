52aa2f1101d30141da9792499dc6ac27d2c9d7ea
김종범 <kbumk1234@naver.com>
2024-04-01 09:46:32 +0900

    fix: 탈퇴 이용자 익명 처리

--- backend/board_service/src/main/java/com/sobolaw/feign/service/UserServiceClient.java
+++ backend/board_service/src/main/java/com/sobolaw/feign/service/UserServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "user-service", path = "/api/user-service", fallback = UserServiceClientFallback.class)
-//@FeignClient(name = "user-service", url = "https://j10a604.p.ssafy.io/api/user-service")
+//@FeignClient(name = "user-service", url = "https://j10a604.p.ssafy.io/api/user-service", fallback = UserServiceClientFallback.class)
 public interface UserServiceClient {
 
     @GetMapping("/members/{memberId}")
