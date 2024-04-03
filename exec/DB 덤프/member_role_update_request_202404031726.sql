36fc0c5e4cebdac8ca63f0a3330f2494d6abf486
김종범 <kbumk1234@naver.com>
2024-04-03 17:32:04 +0900

    docs: db덤프 추가
    Former-commit-id: a5fc1cb8575ef7e5d66b62af09048b96c5898a2b

---
+++ exec/DB 덤프/member_role_update_request_202404031726.sql
@@ -0,0 +1,13 @@
+INSERT INTO s10p22a604.member_role_update_request (id,created_time,is_deleted,updated_time,belong_document_path,original_file_name,member_member_id) VALUES
+	 (1,'2024-04-03 02:18:28.635373',1,'2024-04-03 02:18:50.911958','https://sobolaw.s3.ap-northeast-2.amazonaws.com/d69a6374-c6e2-48bf-bc69-7ca035323f7f.jpg','20240317_205125.jpg',38),
+	 (2,'2024-04-03 02:20:36.876219',1,'2024-04-03 02:21:09.473125','https://sobolaw.s3.ap-northeast-2.amazonaws.com/b09ac709-33aa-465c-81bc-34b563dc526e.jpg','1711191105555.jpg',39),
+	 (3,'2024-04-03 09:14:58.171377',1,'2024-04-03 09:15:12.685559','https://sobolaw.s3.ap-northeast-2.amazonaws.com/e557b804-c2ae-471b-bbcd-2f5a27b9e586.jpeg','ㅈㅈㅅ.jpeg',22),
+	 (4,'2024-04-03 09:37:51.154267',1,'2024-04-03 09:39:10.330232','https://sobolaw.s3.ap-northeast-2.amazonaws.com/180cf584-01ff-41b0-883e-5a444a433e1c.jpg',')P&2@KJ.jpg',40),
+	 (5,'2024-04-03 09:48:15.565181',1,'2024-04-03 09:49:29.249606','https://sobolaw.s3.ap-northeast-2.amazonaws.com/29df6cde-8218-40a1-b601-c32691a16386.jpg',')P&2@KJ.jpg',41),
+	 (6,'2024-04-03 09:51:56.189619',1,'2024-04-03 09:53:57.155255','https://sobolaw.s3.ap-northeast-2.amazonaws.com/2b1dd120-f5bd-4f67-995c-d0201517d159.jpg',')P&2@KJ.jpg',42),
+	 (7,'2024-04-03 09:58:16.826157',1,'2024-04-03 09:59:04.848825','https://sobolaw.s3.ap-northeast-2.amazonaws.com/d9700243-1482-4a5e-83b5-b7725d5935e2.jpg',')P&2@KJ.jpg',43),
+	 (8,'2024-04-03 10:56:14.499990',1,'2024-04-03 10:56:29.706468','https://sobolaw.s3.ap-northeast-2.amazonaws.com/785d0bb9-e8d8-462a-96a5-9e11ede51537.jpg',')P&2@KJ.jpg',44),
+	 (9,'2024-04-03 11:01:19.366350',1,'2024-04-03 11:02:19.399100','https://sobolaw.s3.ap-northeast-2.amazonaws.com/ff87ec2a-87cf-4f13-b492-4f445b8f71a6.jpg',')P&2@KJ.jpg',45),
+	 (10,'2024-04-03 12:00:45.734075',1,'2024-04-03 12:00:55.566748','https://sobolaw.s3.ap-northeast-2.amazonaws.com/5433d300-860a-4550-b9a5-5a44785d65c2.jpg',')P&2@KJ.jpg',46);
+INSERT INTO s10p22a604.member_role_update_request (id,created_time,is_deleted,updated_time,belong_document_path,original_file_name,member_member_id) VALUES
+	 (11,'2024-04-03 16:24:50.432449',1,'2024-04-03 16:25:24.754608','https://sobolaw.s3.ap-northeast-2.amazonaws.com/b0c25c46-eafe-4094-957a-bbc31929f589.PNG','캡처.PNG',34);
---
+++ exec/DB 덤프/mentor_role_update_request_202404031726.sql
@@ -0,0 +1,11 @@
+INSERT INTO s10p22a604.mentor_role_update_request (id,created_time,is_deleted,updated_time,belong_document_path,original_file_name,member_member_id) VALUES
+	 (1,'2024-03-29 18:13:51.741452',1,'2024-04-01 00:24:22.676242','https://sobolaw.s3.ap-northeast-2.amazonaws.com/c86a1c62-5732-4bfa-a0e5-b97caa818096.png','1.png',18),
+	 (2,'2024-03-31 21:15:51.757060',1,'2024-03-31 21:28:34.848369','https://sobolaw.s3.ap-northeast-2.amazonaws.com/ab05f493-cf1a-47e1-b594-9b5dc3f48507.jpg','남해수산.jpg',28),
+	 (11,'2024-04-01 01:55:17.909011',1,'2024-04-01 01:55:17.909011','https://sobolaw.s3.ap-northeast-2.amazonaws.com/b9691bec-45ba-4f9c-8bb5-40e3058cb3fc.png','vege.png',29),
+	 (15,'2024-04-01 02:05:42.774005',1,'2024-04-01 02:05:42.774005','https://sobolaw.s3.ap-northeast-2.amazonaws.com/6f0b46eb-92b3-4670-b094-dfaca88b9b3a.jpg','남해수산.jpg',30),
+	 (17,'2024-04-01 02:08:47.579366',1,'2024-04-01 02:08:47.579366','https://sobolaw.s3.ap-northeast-2.amazonaws.com/ce7c64e0-a4f4-4b30-bb9b-9e0b3591d16e.jpg','남해수산.jpg',31),
+	 (18,'2024-04-01 02:12:41.992628',1,'2024-04-01 02:12:41.992628','https://sobolaw.s3.ap-northeast-2.amazonaws.com/8d32130d-669d-4271-b183-fcffc25e6bdd.jpg','남해수산.jpg',32),
+	 (20,'2024-04-01 02:14:13.443539',1,'2024-04-01 06:11:24.206951','https://sobolaw.s3.ap-northeast-2.amazonaws.com/7747afca-9cfa-4024-a4cf-bdd5bfb3615e.jpg','남해수산.jpg',33),
+	 (21,'2024-04-01 11:54:34.325544',1,'2024-04-01 12:21:23.133378','https://sobolaw.s3.ap-northeast-2.amazonaws.com/bedcf851-ebce-4fed-8440-f9340225975e.png','1920x1080-001.png',8),
+	 (23,'2024-04-01 06:12:31.394869',1,'2024-04-01 06:35:29.276960','https://sobolaw.s3.ap-northeast-2.amazonaws.com/0f4a5fc4-dc12-410c-ab30-2e3992853f20.jpg','남해수산.jpg',36),
+	 (24,'2024-04-01 11:47:28.809951',1,'2024-04-01 15:00:07.421147','https://sobolaw.s3.ap-northeast-2.amazonaws.com/e639c5b2-dc4c-4c7b-8f99-c4c47e007f7f.PNG','캡처.PNG',37);
